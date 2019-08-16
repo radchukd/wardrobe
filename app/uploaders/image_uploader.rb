ecfd555fd84061562fb31b8f69d7fb030ca1960c
radchukd <radchuk.d@gmail.com>
2019-08-16 19:43:04 +0200

    split views to partials

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,6 +37,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [350, 350]
   end
 
+  version :carousel do
+    process resize_to_fit: [1440, 680]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
